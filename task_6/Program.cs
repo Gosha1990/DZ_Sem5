2e0375a8c5606932677717da63f6202e752577a6
Gosha1990 <123661130+Gosha1990@users.noreply.github.com>
2023-02-13 13:12:09 +0300

    6 задача

---
+++ task_6/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Вводим любое число");
+int Number = Convert.ToInt32(Console.ReadLine());
+if(Number % 2 == 0)
+Console.WriteLine("Является четным числом ");
+    else
+    {
+    Console.WriteLine("НЕ Является четным числом ");
+    }
+
